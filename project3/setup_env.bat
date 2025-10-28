0fc1fa761f6ca068150fede245f6626a9ad03fe9
Malik Akhmet <lunarvoyageer@gmail.com>
2025-10-28 13:16:33 +0500

    first commit

--- project3/setup_env.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Создаю виртуальное окружение...
-python -m venv .venv
-call .venv\Scripts\activate
-echo Устанавливаю зависимости...
-pip install -r requirements.txt
-echo Готово! Окружение установлено.
-pause
