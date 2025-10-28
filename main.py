4b2587265dde545d09b1c5029807ecde76af6a71
Malik Akhmet <lunarvoyageer@gmail.com>
2025-10-28 16:53:29 +0500

    recoded main.py

--- main.py
+++ main.py
@@ -1,66 +1,45 @@
-import cv2
-import numpy as np
+import io
 import streamlit as st
-from tensorflow.keras.applications.mobilenet_v2 import (
-    MobileNetV2,
-    preprocess_input,
-    decode_predictions
-)
 from PIL import Image
-# cd project3
-# uv run streamlit run main.py
+import numpy as np
+from tensorflow.keras.applications import EfficientNetB0
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.efficientnet import preprocess_input, decode_predictions
 
 def load_model():
-    model = MobileNetV2(weights='imagenet')
+    model = EfficientNetB0(weights='imagenet')
     return model
 
-def preprocess_image(image):
-    img = np.array(image)
-    img = cv2.resize(img, (224, 224))
-    img = preprocess_input(img)
-    img = np.expand_dims(img, axis=0)
-    return img
+def preprocess_image(img):
+    img = img.resize((224, 224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    return x
 
-def classify_image(model, image):
-    try:
-        processed_image = preprocess_image(image)
-        predictions = model.predict(processed_image)
-        decoded_predictions = decode_predictions(predictions, top=3)[0]
 
-        return decoded_predictions
-    except Exception as e:
-        st.error(f"Error classifying image: {str(e)}")
-        return None
-
-def main():
-    st.set_page_config(page_title='AI Image Classifier', page_icon='🖼', layout = 'centered')
-
-    st.title('AI Image Classifier')
-    st.write('Upload an image and let AI tell you what is in it!')
-
-    @st.cache_resource
-    def load_cached_model():
-        return load_model()
-    
-    model = load_cached_model()
-
-    uploaded_file = st.file_uploader('Choose an image...', type=['jpg', 'png', 'webp'])
-
+def load_image():
+    uploaded_file = st.file_uploader(label='Выберите изображение для распознавания')
     if uploaded_file is not None:
-        image = st.image(
-            uploaded_file, caption='Uploaded Image', use_container_width = True
-        )
-        btn = st.button('Classify Image')
+        image_data = uploaded_file.getvalue()
+        st.image(image_data)
+        return Image.open(io.BytesIO(image_data))
+    else:
+        return None
+    
+def print_predictions(preds):
+    classes = decode_predictions(preds, top=3)[0]
+    for cl in classes:
+        st.write(cl[1], cl[2])
 
-        if btn:
-            with st.spinner('Analyzing Image...'):
-                image = Image.open(uploaded_file)
-                predictions = classify_image(model, image)
-                
-                if predictions:
-                    st.subheader('Predictions')
-                    for _, label, score in predictions:
-                        st.write(f'**{label}** {score:.2%}')
+model = load_model()
 
-if __name__ == '__main__':
-    main()
+st.title('Классификация изображений')
+img = load_image()
+result = st.button('Распознать изображение')
+
+if result:
+    x = preprocess_image(img)
+    preds = model.predict(x)
+    st.write('**Результаты распознавания:**')
+    print_predictions(preds)
